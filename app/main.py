35b3a47468046ac7cff38c4ea9413f33b53ad748
Oleksii-Hrechukha <Oleksii_Hrechukha@outlook.com>
2026-05-31 12:49:09 +0300

    new_Solution

--- app/main.py
+++ app/main.py
@@ -4,24 +4,17 @@ import fighting_config.battle_class
 
 def battle(knights_config: dict) -> dict:
 
-    both_of_knights = fighting_config.battle_class.KnightsBattle(knights_config)
+    both_of_knights = (fighting_config.
+                       battle_class.KnightsBattle(knights_config))
     # BATTLE PREPARATIONS:
 
-    # lancelot
     lancelot = both_of_knights.knights["lancelot"].show_info()
-    both_of_knights.parametrization_of_knight(lancelot)
-
-    # arthur
     arthur = both_of_knights.knights["arthur"].show_info()
-    both_of_knights.parametrization_of_knight(arthur)
-
-    # mordred
     mordred = both_of_knights.knights["mordred"].show_info()
-    both_of_knights.parametrization_of_knight(mordred)
-
-    # red_knight
     red_knight = both_of_knights.knights["red_knight"].show_info()
-    both_of_knights.parametrization_of_knight(red_knight)
+
+    for knight in [lancelot, arthur, mordred, red_knight]:
+        both_of_knights.parametrization_of_knight(knight)
 
     # -------------------------------------------------------------------------------
     # BATTLE:
--- fighting_config/battle_class.py
+++ fighting_config/battle_class.py
@@ -1,7 +1,9 @@
+from typing import Any
+
+
 class KnightsParameters:
 
-    def __init__(self, knight, param) -> None:
-        self.knight = knight
+    def __init__(self, param: Any) -> None:
         self.name = param["name"]
         self.power = param["power"]
         self.hp = param["hp"]
@@ -10,14 +12,13 @@ class KnightsParameters:
         self.potion = param["potion"]
 
     def show_info(self) -> dict:
-
         dict_of_param = {
-                "name": self.name,
-                "power": self.power,
-                "hp": self.hp,
-                "armour": self.armour,
-                "weapon": self.weapon,
-                "potion": self.potion,
+            "name": self.name,
+            "power": self.power,
+            "hp": self.hp,
+            "armour": self.armour,
+            "weapon": self.weapon,
+            "potion": self.potion,
         }
         return dict_of_param
 
@@ -28,35 +29,30 @@ class KnightsBattle:
         self.knights = {}
 
         for knight, param in knights_dict.items():
-            self.knights[knight] = KnightsParameters(knight, param)
-
+            self.knights[knight] = KnightsParameters(param)
 
     def parametrization_of_knight(self, param_dict: dict) -> dict:
-
         param_dict["protection"] = 0
-        for a in param_dict["armour"]:
-            param_dict["protection"] += a["protection"]
-
+        for arm in param_dict["armour"]:
+            param_dict["protection"] += arm["protection"]
         # apply weapon
         param_dict["power"] += param_dict["weapon"]["power"]
 
         # apply potion if exist
         if param_dict["potion"] is not None:
-            if "power" in param_dict["potion"]["effect"]:
-                param_dict["power"] += param_dict["potion"]["effect"]["power"]
-
-            if "protection" in param_dict["potion"]["effect"]:
-                param_dict["protection"] += param_dict["potion"]["effect"]["protection"]
-
-            if "hp" in param_dict["potion"]["effect"]:
-                param_dict["hp"] += param_dict["potion"]["effect"]["hp"]
+            potion = param_dict["potion"]["effect"]
+            param_dict["power"] += potion.get("power", 0)
+            param_dict["protection"] += potion.get("protection", 0)
+            param_dict["hp"] += potion.get("hp", 0)
         return param_dict
 
+    def knights_battle(self, first_fighter: dict,
+                       second_fighter: dict) -> None:
 
-    def knights_battle(self, first_fighter: dict, second_fighter: dict) -> None:
-
-        first_fighter["hp"] -= second_fighter["power"] - first_fighter["protection"]
-        second_fighter["hp"] -= first_fighter["power"] - second_fighter["protection"]
+        first_fighter["hp"] -= (
+            second_fighter)["power"] - first_fighter["protection"]
+        second_fighter["hp"] -= (
+            first_fighter)["power"] - second_fighter["protection"]
 
         # check if someone fell in battle
         if first_fighter["hp"] <= 0:
